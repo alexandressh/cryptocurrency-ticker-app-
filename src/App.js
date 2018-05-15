cf2a3a4db8f0d2bc70b46cd4dd4527ea1a42991a
Henrique Alexandre <alexandressh@gmail.com>
2018-05-15 20:05:51 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import Tickers from './components/Tickers';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Cryptocurrency Ticker</h2>
+        </header>
+        <Tickers />
+      </div>
+    );
+  }
+}
+
+export default App;
